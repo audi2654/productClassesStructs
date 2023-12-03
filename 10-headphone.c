9734ce7aef0cd9b6e02b8a22b98cb70c6050dd27
audi2654 <adit2654@gmail.com>
2023-12-03 21:59:35 +0530

    new classes

--- 10-headphone.c
+++ 10-headphone.c
@@ -1,5 +1,6 @@
 //C Struct for Beyerdynamic Headphone
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef int iBool_t;
 
@@ -63,6 +64,10 @@ void showProductDetails(const struct productProps* productVariable);
 
 static void* ampMalloc(size_t sizeOfStruct);
 
+char* cYesOrNo(const _Bool* member); 
+
+char* c_cYesOrNo(const iBool_t* member);
+
 int main(void)
 {
     struct productProps* newProduct = (struct productProps*)ampMalloc(sizeof(struct productProps));
@@ -91,7 +96,7 @@ int main(void)
         2.5, 3.5, 10.0,
         (struct Date){25, "July", 2015},
         NO,
-        NO,
+        YES,
         NO,
         YES,
         NO,
@@ -105,4 +110,88 @@ int main(void)
     showProductDetails(newProduct);
 
     return(0);
+}
+
+//function definitions
+void setProductDetails
+(
+    struct productProps* structVariableToSetValues,
+    char* _pcName,
+    unsigned int _uiPrice,
+    float _fHeight,
+    float _fWidth,
+    float _fLength,
+    struct Date _structDate_DateFirstAvailable,
+    _Bool _bBatteriesIncluded,
+    _Bool _bBatteriesRequired,
+    _Bool _bContainsLiquid,
+    iBool_t _bAcAdaptor,
+    iBool_t _bIncludesRemote,
+    unsigned short _usGsmFrequency,
+    char* _pcModelName,
+    unsigned int _uiWeight,
+    char* _pcCountryOfOrigin,
+    float _fCustomerRatings
+)
+{
+    structVariableToSetValues->pcName = _pcName;
+    structVariableToSetValues->uiPrice = _uiPrice;
+    structVariableToSetValues->structDim_DimensionsOfProduct = (struct Dimensions){_fLength, _fWidth, _fHeight};
+    structVariableToSetValues->structDate_DateFirstAvailable = _structDate_DateFirstAvailable;
+    structVariableToSetValues->bBatteriesIncluded = _bBatteriesIncluded;
+    structVariableToSetValues->bBatteriesRequired = _bBatteriesRequired;
+    structVariableToSetValues->bContainsLiquid = _bContainsLiquid;
+    structVariableToSetValues->bAcAdaptor = _bAcAdaptor;
+    structVariableToSetValues->bIncludesRemote = _bIncludesRemote;
+    structVariableToSetValues->usGsmFrequency = _usGsmFrequency;
+    structVariableToSetValues->pcModelName = _pcModelName;
+    structVariableToSetValues->uiWeight = _uiWeight;
+    structVariableToSetValues->pcCountryOfOrigin = _pcCountryOfOrigin;
+    structVariableToSetValues->fCustomerRatings = _fCustomerRatings;
+}
+
+void showProductDetails(const struct productProps* productVariable)
+{
+    printf("%s\n", productVariable->pcName);
+    printf("%i\n", productVariable->uiPrice);
+    printf("%0.1fx%0.1fx%0.1f\n", productVariable->structDim_DimensionsOfProduct.fHeight, productVariable->structDim_DimensionsOfProduct.fLength, productVariable->structDim_DimensionsOfProduct.fWidth);
+    printf("%hu-%s-%hu\n", productVariable->structDate_DateFirstAvailable.usDay, productVariable->structDate_DateFirstAvailable.pcMonth, productVariable->structDate_DateFirstAvailable.usYear);
+    printf("%s\n", cYesOrNo(&productVariable->bBatteriesIncluded));
+    printf("%s\n", cYesOrNo(&productVariable->bBatteriesRequired));
+    printf("%s\n", cYesOrNo(&productVariable->bContainsLiquid));
+    printf("%s\n", c_cYesOrNo(&productVariable->bAcAdaptor));
+    printf("%s\n", c_cYesOrNo(&productVariable->bIncludesRemote));
+    printf("%hu\n", productVariable->usGsmFrequency);
+    printf("%s\n", productVariable->pcModelName);
+    printf("%hu\n", productVariable->uiWeight);
+    printf("%s\n", productVariable->pcCountryOfOrigin);
+    printf("%0.3f\n", productVariable->fCustomerRatings);
+
+}
+
+static void* ampMalloc(size_t sizeOfStruct)
+{
+    size_t* p = NULL;
+    p = (void*)malloc(sizeOfStruct);
+    if(p == NULL)
+    {
+        puts("Allocating memory failed in ampMalloc()\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return(p);
+}
+
+char* cYesOrNo(const _Bool* member)
+{
+    if(*member == YES)
+        return("YES");
+    return("NO");
+}
+
+char* c_cYesOrNo(const iBool_t* member)
+{
+    if(*member == YES)
+        return("YES");
+    return("NO");
 }
--- 10-headphone.cpp
+++ 10-headphone.cpp
@@ -1,30 +1,153 @@
 //C++ Class for Beyerdynamic Headphone
 #include <iostream>
 
-class Dimensions{
-
-};
-
-class Date{
-
-};
+const bool YES = 1;
+const bool NO = 0;
 
 class productProps{
     private:
     const char* pcName;
+    unsigned int uiPrice;
+    unsigned short usGsmFrequency;
+    const char* pcModelName;
+    unsigned int uiWeight;
+    const char* pcCountryOfOrigin;
+    float fCustomerRatings;
+
+    struct allYesOrNoProps;
+    
+    class Dimensions{
+        private:
+        float fHeight;
+        float fWidth;
+        float fLength;
+
+        public:
+        Dimensions(float _h, float _w, float _l)
+        :   fHeight(_h), fWidth(_w), fLength(_l)
+        {
+
+        }
+    } dimensionsOfProduct;
+
 
     public:
+    class Date;
 
-    void setProductDetails();
+    //default constr
+    productProps()
+    :   pcName{"empty_name"},
+        uiPrice{0},
+        usGsmFrequency{0},
+        pcModelName{"empty_name"},
+        uiWeight{0},
+        pcCountryOfOrigin{"empty_country"},
+        fCustomerRatings{0},
+        dimensionsOfProduct{0.0,0.0,0.0}
+    {
+
+    }
+    
+    //function declarations
+    void setProductDetails
+    (
+        const char* _pcName,
+        unsigned int _uiPrice,
+        Dimensions _dimensionsOfProduct,
+        Date _dateFirstAvailable,
+        bool _bBatteriesIncluded,
+        bool _bBatteriesRequired,
+        bool _bContainsLiquid,
+        bool _bAcAdaptor,
+        bool _bIncludesRemote,
+        unsigned short _usGsmFrequency,
+        const char* _pcModelName,
+        unsigned int _uiWeight,
+        const char* _pcCountryOfOrigin,
+        float _fCustomerRatings
+    );
     void showProductDetails();
 };
 
 int main(void){
     productProps* newProduct = new productProps;
 
-    newProduct->setProductDetails();
+    newProduct->setProductDetails
+    (
+        "Beyerdynamic Headphone",
+        12989,
+        {2.5, 3.5, 10.0},
+        {25, "July", 2015},
+        NO,
+        YES,
+        NO,
+        YES,
+        NO,
+        35000,
+        "DT 770 Pro 80",
+        96,
+        "Germany",
+        4.7
+    );
 
     newProduct->showProductDetails();
 
     return 0;
+}
+
+//definitions
+class productProps::Date{
+    private:
+    unsigned short usDay;
+    const char* pcMonth;
+    unsigned short usYear;
+
+    public:
+    Date(unsigned short _d = 0, const char* _m = "empty", unsigned short _y = 0000)
+    :   usDay(_d), pcMonth(_m), usYear(_y)
+    {
+
+    }
+} dateFirstAvailable;
+
+struct productProps::allYesOrNoProps{
+    bool bBatteriesIncluded;
+    bool bBatteriesRequired;
+    bool bContainsLiquid;
+    bool bAcAdaptor;
+    bool bIncludesRemote;
+} bAllYesOrNoProps = {NO, NO, NO, NO, NO};
+
+void productProps::setProductDetails
+(
+    const char* _pcName,
+    unsigned int _uiPrice,
+    Dimensions _dimensionsOfProduct,
+    Date _dateFirstAvailable,
+    bool _bBatteriesIncluded,
+    bool _bBatteriesRequired,
+    bool _bContainsLiquid,
+    bool _bAcAdaptor,
+    bool _bIncludesRemote,
+    unsigned short _usGsmFrequency,
+    const char* _pcModelName,
+    unsigned int _uiWeight,
+    const char* _pcCountryOfOrigin,
+    float _fCustomerRatings
+)
+{
+    this->pcName = _pcName;
+    uiPrice = _uiPrice;
+    dimensionsOfProduct = _dimensionsOfProduct;
+    dateFirstAvailable = _dateFirstAvailable;
+    bAllYesOrNoProps.bBatteriesIncluded = _bBatteriesIncluded;
+    bAllYesOrNoProps.bBatteriesRequired = _bBatteriesRequired;
+    bAllYesOrNoProps.bContainsLiquid = _bContainsLiquid;
+    bAllYesOrNoProps.bAcAdaptor = _bAcAdaptor;
+    bAllYesOrNoProps.bIncludesRemote = _bIncludesRemote;
+    usGsmFrequency = _usGsmFrequency;
+    pcModelName = _pcModelName;
+    uiWeight = _uiWeight;
+    pcCountryOfOrigin = _pcCountryOfOrigin;
+    fCustomerRatings = _fCustomerRatings;
 }
